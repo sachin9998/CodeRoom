c5be6562b578469f806a5ec11270dba880ac25a2
Sachin Alam <sachinalam4216@gmail.com>
2025-01-15 15:49:03 +0530

    frontend phase 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+// import Editor from "./pages/Editor";
+// import Login from "./pages/Login";
+import NoPage from "./pages/NoPage";
+import Home from "./pages/Home";
+// import SignUp from "./pages/SignUp";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  return (
+    <BrowserRouter>
+      <RouteHandler />
+    </BrowserRouter>
+  );
+};
+
+const RouteHandler = () => {
+  // const isLoggedIn = localStorage.getItem("isLoggedIn");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/signUp" element={<SignUp />} />
+      <Route path="/login" element={<Login />} /> */}
+      {/* <Route
+        path="/editior/:id"
+        element={isLoggedIn ? <Editor /> : <Navigate to={"/login"} />}
+      /> */}
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
