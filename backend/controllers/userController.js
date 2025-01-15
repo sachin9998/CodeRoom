5fa6f39024d7f10e4c55c3aff6f3bb8464eba443
Sachin Alam <sachinalam4216@gmail.com>
2025-01-15 10:48:50 +0530

    user model added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+export const signUp = (req, res) => {
+  try {
+    // code to create a user
+
+    
+
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from "express";
+import { signUp } from "../controllers/userController";
+var router = Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-module.exports = router;
+router.post("/signUp", signUp);
+
+export default router;
