cb1d47aa2a4ea657640b28c84b9fd730f135c3fa
Sachin Alam <sachinalam4216@gmail.com>
2025-01-15 10:34:17 +0530

    first push

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import createError from "http-errors";
+import logger from "morgan";
+import path from "path";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+const app = express();
+
+// __dirname equivalent in ES Modules
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
