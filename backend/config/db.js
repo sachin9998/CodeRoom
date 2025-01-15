ca2cbf669beecf93684a977c9fd29b40a3f2e00c
Sachin Alam <sachinalam4216@gmail.com>
2025-01-15 10:37:35 +0530

    connect db added

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,15 @@
 import mongoose from "mongoose";
 
-const 
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/codeIDE", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
