4e02b4a7d227df55e3f8e522e8eda844b5913d91
Sachin Alam <sachinalam4216@gmail.com>
2025-01-16 13:14:13 +0530

    removed cors

--- backend/app.js
+++ backend/app.js
@@ -28,13 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
-app.use(
-  cors({
-    origin: "https://coderoom-five.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
